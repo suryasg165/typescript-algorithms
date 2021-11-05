1f5a2025f2c33eed472bfe77677d3bf3626eece9
suryasg165 <suryanarayana.pabbineedi@senecaglobal.com>
2021-11-05 20:27:00 +0530

    1 file modified

--- string-operations.ts
+++ string-operations.ts
@@ -1,3 +1,23 @@
 export const reverseString = (incomingString: string) => {
-  return incomingString.split('').reverse();
+  return incomingString.split('').reverse().join('');
+};
+
+export const reverseString_ = (incomingString: string) => {
+  let reversed = '';
+
+  for (let char of incomingString) {
+    reversed = char + reversed;
+  }
+
+  return reversed;
+};
+
+export const reverseString__ = (incomingString: string) => {
+  return incomingString.split('').reduce((result, char) => char + result);
+};
+
+export function test(incomingString: string) {
+  console.log(incomingString, reverseString(incomingString));
+  console.log(incomingString, reverseString_(incomingString));
+  console.log(incomingString, reverseString__(incomingString));
 }
